984144170d2e4c2da47ea169543ae02c47879fef
stcalica <kycalica@gmail.com>
2017-02-16 11:41:58 +0000

    added dates to backup

--- backup_site.sh
+++ backup_site.sh
@@ -1,7 +1,9 @@
-cd ./backup 
+cd ./backup
 
-docker run --rm --volumes-from edgeofrevolt_wordpress_1 -v $(pwd):/backup wordpress tar zcvf /backup/edgeofrevolt_wordpress.tar.gz /var/www/html
+date=$(date "+%F-%T") 
 
-docker exec -i edgeofrevolt_db_1 mysqldump -uroot -pwordpress wordpress > edgeofrevolt_mysql.sql
+docker run --rm --volumes-from edgeofrevolt_wordpress_1 -v $(pwd):/backup wordpress tar zcvf /backup/edgeofrevolt_wordpress_$date.tar.gz /var/www/html
+
+docker exec -i edgeofrevolt_db_1 mysqldump -uroot -pwordpress wordpress > edgeofrevolt_mysql_$date.sql
 
 
