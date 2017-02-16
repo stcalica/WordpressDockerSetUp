537e393a305c447cfb742c3b9f407fdc7f97a971
stcalica <kycalica@gmail.com>
2017-02-16 14:35:42 +0000

    upped limit

--- backup_site.sh
+++ backup_site.sh
@@ -1,9 +1,11 @@
 cd ./backup
 
 date=$(date "+%F-%T") 
+sqlpasswd= 
+
 
 docker run --rm --volumes-from edgeofrevolt_wordpress_1 -v $(pwd):/backup wordpress tar zcvf /backup/edgeofrevolt_wordpress_$date.tar.gz /var/www/html
 
-docker exec -i edgeofrevolt_db_1 mysqldump -uroot -pwordpress wordpress > edgeofrevolt_mysql_$date.sql
+docker exec -i edgeofrevolt_db_1 mysqldump -uroot -p$sqlpasswd wordpress > edgeofrevolt_mysql_$date.sql
 
 
